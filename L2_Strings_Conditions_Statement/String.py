c82c2804be6acabd2d57fbada8f994f414eabcf6
Kaustubh-06 <0241ECE110@niet.co.in>
2025-07-10 12:35:56 +0530

    New File

---
+++ L2_Strings_Conditions_Statement/String.py
@@ -0,0 +1,18 @@
+#String(Alphabets)
+str = "Hello" # Output: Hello
+str2 = 'Hi' # Output: Hi
+str3 = '''Hey''' #  Output: Hey 
+
+# It helps to create multi-line strings.
+str4 = """Hello
+World""" # Output: Hello World
+#It helps to write apstorophes in strings.
+str5 = "It's a beautiful day" # Output: It's a beautiful day
+str6 = 'She said, "Hello!"' # Output: She said, "Hello!"
+
+str7 = "Hello" + " " + "World" # Output: Hello World
+str8 = "Hello" * 3 # Output: HelloHelloHello
+str9 = "This is a string.\nThis is a new line." 
+# Output: This is a string.
+# This is a new line.
+str10 = "This is a string.\tThis is a tab." # Output: This is a string.    This is a tab.
